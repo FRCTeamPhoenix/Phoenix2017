51dfd7c54c1dd3b5bd0f68e13bb5acfedc460e97
Ben NF <erogone775@gmail.com>
2017-01-10 18:57:42 -0500

    uncommented logger

--- src/LoggerController.cpp
+++ src/LoggerController.cpp
@@ -1,63 +1,63 @@
-///*
-// * LoggerController.cpp
-// *
-// *  Created on: Oct 17, 2016
-// *      Author: connortess
-// */
-//
-//#include <LoggerController.h>
-//
-//LoggerController::LoggerController()
-//{
+/*
+ * LoggerController.cpp
+ *
+ *  Created on: Oct 17, 2016
+ *      Author: connortess
+ */
+
+#include <LoggerController.h>
+
+LoggerController::LoggerController()
+{
 //    initLogger();
-//}
-//
-//LoggerController::~LoggerController()
-//{
-//    // TODO Auto-generated destructor stub
-//}
-//
-//void initLogger(){
-//    //Logger initialized with Debug severity
-//    plog::init(plog::debug, "/home/lvuser/robot_log.txt");
-//
-//    LOGI << "Robot initialized";
-//}
-//
-//void LoggerController::setSeverity(plog::Severity severity){
-//
-//    /* Logger Severity Levels, displays all levels below set severity level
-//     *      none = 0,
-//            fatal = 1,
-//            error = 2,
-//            warning = 3,
-//            info = 4,
-//            debug = 5,
-//            verbose = 6
-//     */
-//
-//    plog::get()->setMaxSeverity(severity);
-//    LOGF << "Severity Change: Severity changed to " << plog::severityToString(severity);
-//}
-//
-//void LoggerController::updateSeverity(){
-//    //Interfacing in labview on the driver station
-//    int place = (int)SmartDashboard::GetNumber("Level of Severity", -1);
-//
-//    plog::Severity severity = (plog::Severity) place;
-//
-//    if (severity != plog::get()->getMaxSeverity()) {
-//        LOGF << "Severity Change: Severity changed to " << plog::severityToString(severity);
-//    }
-//
-//    plog::get()->setMaxSeverity(severity);
-//}
-//
-//string LoggerController::getSeverityString(){
-//    return plog::severityToString(plog::get()->getMaxSeverity());
-//
-//}
-//
-//int LoggerController::getSeverityInt(){
-//    return (int)plog::get()->getMaxSeverity();
-//}
+}
+
+LoggerController::~LoggerController()
+{
+    // TODO Auto-generated destructor stub
+}
+
+void initLogger(){
+    //Logger initialized with Debug severity
+    plog::init(plog::debug, "/home/lvuser/robot_log.txt");
+
+    LOGI << "Robot initialized";
+}
+
+void LoggerController::setSeverity(plog::Severity severity){
+
+    /* Logger Severity Levels, displays all levels below set severity level
+     *      none = 0,
+            fatal = 1,
+            error = 2,
+            warning = 3,
+            info = 4,
+            debug = 5,
+            verbose = 6
+     */
+
+    plog::get()->setMaxSeverity(severity);
+    LOGF << "Severity Change: Severity changed to " << plog::severityToString(severity);
+}
+
+void LoggerController::updateSeverity(){
+    //Interfacing in labview on the driver station
+    int place = (int)SmartDashboard::GetNumber("Level of Severity", -1);
+
+    plog::Severity severity = (plog::Severity) place;
+
+    if (severity != plog::get()->getMaxSeverity()) {
+        LOGF << "Severity Change: Severity changed to " << plog::severityToString(severity);
+    }
+
+    plog::get()->setMaxSeverity(severity);
+}
+
+string LoggerController::getSeverityString(){
+    return plog::severityToString(plog::get()->getMaxSeverity());
+
+}
+
+int LoggerController::getSeverityInt(){
+    return (int)plog::get()->getMaxSeverity();
+}
--- src/LoggerController.h
+++ src/LoggerController.h
@@ -1,42 +1,42 @@
-///*
-// * LoggerController.h
-// *
-// *  Created on: Oct 17, 2016
-// *      Author: connortess
-// */
-//
-//#ifndef SRC_PLOG_LOGGERCONTROLLER_H_
-//#define SRC_PLOG_LOGGERCONTROLLER_H_
-//#include "WPILib.h"
-//#include "plog/Record.h"
-//#include "plog/Severity.h"
-//#include "plog/Util.h"
-//#include "plog/Init.h"
-//#include "plog/Log.h"
-//#include "plog/Logger.h"
-//#include <string>
-//#include <iostream>
-//using namespace std;
-//
-//class LoggerController
-//{
-//public:
-//    LoggerController();
-//    virtual ~LoggerController();
-//
-//
-//    //updates severity from the driver station
-//    void updateSeverity();
-//
-//    //sets the severity based on user input
-//    void setSeverity(plog::Severity);
-//
-//    int getSeverityInt();
-//
-//    string getSeverityString();
-//
-//    void initLogger();
-//
-//};
-//
-//#endif /* SRC_PLOG_LOGGERCONTROLLER_H_ */
+/*
+ * LoggerController.h
+ *
+ *  Created on: Oct 17, 2016
+ *      Author: connortess
+ */
+
+#ifndef SRC_PLOG_LOGGERCONTROLLER_H_
+#define SRC_PLOG_LOGGERCONTROLLER_H_
+#include "WPILib.h"
+#include "plog/Record.h"
+#include "plog/Severity.h"
+#include "plog/Util.h"
+#include "plog/Init.h"
+#include "plog/Log.h"
+#include "plog/Logger.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class LoggerController
+{
+public:
+    LoggerController();
+    virtual ~LoggerController();
+
+
+    //updates severity from the driver station
+    void updateSeverity();
+
+    //sets the severity based on user input
+    void setSeverity(plog::Severity);
+
+    int getSeverityInt();
+
+    string getSeverityString();
+
+    void initLogger();
+
+};
+
+#endif /* SRC_PLOG_LOGGERCONTROLLER_H_ */
