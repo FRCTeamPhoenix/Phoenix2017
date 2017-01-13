ddf52b7c303aa2c822456b412a0ed3ec3bb99440
Brin Harper <brin.harper@team2342.org>
2017-01-12 20:40:53 -0500

    Added and configured ShooterCalibrator.cpp file

---
+++ src/ShooterCalibrator.cpp
@@ -0,0 +1,26 @@
+/*
+ * ShooterCalibrator.cpp
+ *
+ * Implements interpolation of necessary power to apply to the shooter's flywheels,
+ * based on distance from target.
+ *
+ *  Created on: Jan 12, 2017
+ *      Author: Brin Harper
+ */
+
+#include <ShooterCalibrator.h>
+
+ShooterCalibrator::ShooterCalibrator()
+{
+
+}
+
+double ShooterCalibrator::getFlywheelPower(double distance)
+{
+   return 0;
+}
+
+ShooterCalibrator::~ShooterCalibrator()
+{
+
+}
