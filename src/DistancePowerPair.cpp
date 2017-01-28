e1a11b36cd451b6009aab4da803d26e3e243c5d9
Brin Harper <brin.harper@team2342.org>
2017-01-28 16:05:54 -0500

    Added json library and started DistancePowerPair class

---
+++ src/DistancePowerPair.cpp
@@ -0,0 +1,12 @@
+/*
+ * DistancePowerPair.cpp
+ *
+ * Stores a distance/power pair in an organized manner.
+ *
+ *  Created on: Jan 28, 2017
+ *      Author: Brin Harper
+ */
+
+#include <DistancePowerPair.h>
+
+
---
+++ src/DistancePowerPair.h
@@ -0,0 +1,27 @@
+/*
+ * DistancePowerPair.h
+ *
+ * Stores a distance/power pair in an organized manner.
+ *
+ *  Created on: Jan 28, 2017
+ *      Author: Brin Harper
+ */
+
+#include "WPILib.h"
+#ifndef SRC_DISTANCEPOWERPAIR_H_
+#define SRC_DISTANCEPOWERPAIR_H_
+
+class DistancePowerPair
+{
+
+    public:
+        DistancePowerPair();
+
+    private:
+
+
+};
+
+
+
+#endif /* SRC_DISTANCEPOWERPAIR_H_ */
