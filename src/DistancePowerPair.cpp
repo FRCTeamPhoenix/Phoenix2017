5f43e8524aa124f9c4cd07a20f73fe8df83a0555
Brin Harper <brin.harper@team2342.org>
2017-01-28 16:52:12 -0500

    Begin to implement DistancePowerPair class

--- src/DistancePowerPair.cpp
+++ src/DistancePowerPair.cpp
@@ -9,4 +9,35 @@
 
 #include <DistancePowerPair.h>
 
+//        DistancePowerPair(double d, double p);
+//        DistancePowerPair(json points);
+//        double getDistance();
+//        void setDistance(double d);
+//        double getPower();
+//        void setPower(double p);
+//        virtual ~DistancePowerPair();
 
+
+// Constructor that takes a distance value and a power value
+//DistancePowerPair::DistancePowerPair(double distance, double power) {
+//
+//    m_distance = distance;
+//    m_power = power;
+//
+//}
+
+// Constructor that takes a json object (implement format checking)
+//DistancePowerPair::DistancePowerPair(json point) {
+//
+//    m_distance = point["distance"];
+//    m_power = point["power"];
+//
+//}
+
+double DistancePowerPair::getDistance() {
+    return m_distance;
+}
+
+void DistancePowerPair::setDistance(double distance) {
+    m_distance = distance;
+}
--- src/DistancePowerPair.h
+++ src/DistancePowerPair.h
@@ -8,17 +8,30 @@
  */
 
 #include "WPILib.h"
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
 #ifndef SRC_DISTANCEPOWERPAIR_H_
 #define SRC_DISTANCEPOWERPAIR_H_
+
+using namespace std;
+using json=nlohmann::json;
 
 class DistancePowerPair
 {
 
     public:
-        DistancePowerPair();
+        DistancePowerPair(double distance, double power);
+        DistancePowerPair(json points);
+        double getDistance();
+        void setDistance(double distance);
+        double getPower();
+        void setPower(double power);
+        virtual ~DistancePowerPair();
 
     private:
-
+        double m_distance;
+        double m_power;
 
 };
 
