cdc14d24b31a90c968d541d5f3590cfcaac243eb
Ian Coolidge <ian.coolidge@asdnh.org>
2017-02-06 20:03:29 -0500

    added maximum i accumulation to indexer

--- src/Indexer.cpp
+++ src/Indexer.cpp
@@ -11,6 +11,7 @@ Indexer::Indexer(SmartTalon& indexerMotor, Joystick& gamepad):
     m_indexerMotor(indexerMotor),
     m_gamepad(gamepad)
 {
+
     m_state = OFF;
 }
 
@@ -32,13 +33,20 @@ void Indexer::run()
 {
     switch(m_state) {
         case TELEOP:
-            if (m_gamepad.GetRawButton(DriveStationConstants::buttonX))
+            if (m_gamepad.GetRawButton(DriveStationConstants::buttonX)) {
                 m_indexerMotor.goAt(1.0);
+
+                if(m_indexerMotor.GetIaccum() > 700000)
+                    m_indexerMotor.ClearIaccum();
+            }
             else
                 m_indexerMotor.goAt(0.0);
             break;
         case ON:
             m_indexerMotor.goAt(1.0);
+
+            if(m_indexerMotor.GetIaccum() > 700000)
+                m_indexerMotor.ClearIaccum();
             break;
         case OFF:
             m_indexerMotor.goAt(0.0);
