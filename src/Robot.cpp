ade3481257b56c7aa23e9387132cf74d17eefa50
Brin Harper <brin.harper@team2342.org>
2017-02-11 13:09:58 -0500

    change test code

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -27,8 +27,8 @@ class Robot: public SampleRobot
 //    SmartTalon m_BRDrive;
 //    SmartTalon m_BLDrive;
 //    relativeMecanumDrivetrain m_drivetrain;
-    SmartTalon m_rightFlyWheelMotor;
-    SmartTalon m_leftFlyWheelMotor;
+    SmartTalon m_topFlyWheelMotor;
+    SmartTalon m_lowerFlyWheelMotor;
     SmartTalon m_turretRotateMotor;
     DigitalInput m_leftLimitSwitch;
     DigitalInput m_rightLimitSwitch;
@@ -52,15 +52,15 @@ public:
 //            m_BRDrive(4, CANTalon::FeedbackDevice::QuadEncoder),
 //            m_BLDrive(1, CANTalon::FeedbackDevice::QuadEncoder),
 //            m_drivetrain(m_FRDrive, m_FLDrive, m_BRDrive, m_BLDrive),
-            m_rightFlyWheelMotor(PortAssign::rightFlyWheelMotor, CANTalon::FeedbackDevice::QuadEncoder),
-            m_leftFlyWheelMotor(PortAssign::leftFlyWheelMotor, CANTalon::FeedbackDevice::QuadEncoder),
+            m_topFlyWheelMotor(PortAssign::rightFlyWheelMotor, CANTalon::FeedbackDevice::QuadEncoder),
+            m_lowerFlyWheelMotor(PortAssign::leftFlyWheelMotor, CANTalon::FeedbackDevice::QuadEncoder),
             m_turretRotateMotor(PortAssign::turret, CANTalon::FeedbackDevice::QuadEncoder),
             m_leftLimitSwitch(PortAssign::leftLimitSwitch),
             m_rightLimitSwitch( PortAssign::rightLimitSwitch),
             m_joystick(PortAssign::joystick),
             m_gamepad(PortAssign::gamepad),
             m_expansionBoard(),
-            m_flywheel(m_rightFlyWheelMotor, m_leftFlyWheelMotor, m_gamepad),
+            m_flywheel(m_topFlyWheelMotor, m_lowerFlyWheelMotor, m_gamepad),
             m_turret(m_turretRotateMotor,m_leftLimitSwitch, m_rightLimitSwitch, m_gamepad),
             m_loggerController(),
             m_shooterController(m_flywheel, m_turret),
@@ -113,94 +113,119 @@ public:
 
         LOGI << "Start Test Mode";
 
-        m_feederMotor.SetControlMode(CANSpeedController::kPercentVbus);
-        m_indexerMotor.SetControlMode(CANSpeedController::kPercentVbus);
+                m_feederMotor.SetControlMode(CANSpeedController::kPercentVbus);
+                m_indexerMotor.SetControlMode(CANSpeedController::kPercentVbus);
 
-        m_rightFlyWheelMotor.SetControlMode(CANSpeedController::kPercentVbus);
-        m_leftFlyWheelMotor.SetControlMode(CANSpeedController::kPercentVbus);
-
-
-        while (IsEnabled() && IsTest())
-        {
-            //A = indexer quarter rotation
-            //B = feeder run of dashboard from slider 0 input
-            //joystick throttle = run feeder
-            //joystick Y = run flywheels
-            //gamepad Y -= run indexer
-            // X = run flywheels off driverstation from slider 1 and 2 input
+                m_topFlyWheelMotor.SetControlMode(CANSpeedController::kPercentVbus);
+                m_lowerFlyWheelMotor.SetControlMode(CANSpeedController::kPercentVbus);
 
 
-           //feeder test
-
-           if (m_gamepad.GetRawButton(DriveStationConstants::buttonB)){
-              m_feederMotor.Set(SmartDashboard::GetNumber("DB/Slider 0",0.0));
-
-           }
-           else
-           {
-               m_feederMotor.Set((m_joystick.GetThrottle()-1) / 2);
-           }
-            std::ostringstream throttleValue;
-            throttleValue << "Throttle: ";
-            throttleValue << ((m_joystick.GetThrottle()-1) / 2);
-            SmartDashboard::PutString("DB/String 7", throttleValue.str());
-
-            std::ostringstream feederEncoderValue;
-            feederEncoderValue << "EncoderF: ";
-            feederEncoderValue << m_feederMotor.GetEncPosition();
-            SmartDashboard::PutString("DB/String 6", feederEncoderValue.str());
+                while (IsEnabled() && IsTest())
+                {
+                    //A = indexer quarter rotation
+                    //B = feeder run of dashboard from slider 0 input
+                    //joystick throttle = run feeder
+                    //joystick Y = run flywheels
+                    //gamepad Y -= run indexer
+                    // X = run flywheels off driverstation from slider 1 and 2 input
 
 
+                   // FEEDER
+                   // B: Run feeder off dashboard from slider 0 input
+                   // Joystick throttle: Run feeder
+                   if (m_gamepad.GetRawButton(DriveStationConstants::buttonB)){
+                      m_feederMotor.Set(SmartDashboard::GetNumber("DB/Slider 0",0.0));
 
-            //indexer test
-            if (m_gamepad.GetRawButton(DriveStationConstants::buttonA)){
-                m_indexerMotor.goDistance(250,0.5);
+                   }
+                   else
+                   {
+                       m_feederMotor.Set((m_joystick.GetThrottle()-1) / 2);
+                   }
+                    std::ostringstream throttleValue;
+                    throttleValue << "Throttle: ";
+                    throttleValue << ((m_joystick.GetThrottle()-1) / 2);
+                    SmartDashboard::PutString("DB/String 7", throttleValue.str());
 
-           }
-            else{
-                m_indexerMotor.Set(m_gamepad.GetY());
-            }
-
-           std::ostringstream indexerEncoderValue;
-           indexerEncoderValue << "EncoderI: ";
-           indexerEncoderValue << m_feederMotor.GetEncPosition();
-           SmartDashboard::PutString("DB/String 8", indexerEncoderValue.str());
+                    std::ostringstream feederEncoderValue;
+                    feederEncoderValue << "EncoderF: ";
+                    feederEncoderValue << m_feederMotor.GetEncPosition();
+                    SmartDashboard::PutString("DB/String 6", feederEncoderValue.str());
 
 
 
-           if (m_gamepad.GetRawButton(DriveStationConstants::buttonX)){
-               m_rightFlyWheelMotor.Set(SmartDashboard::GetNumber("DB/Slider 1",0.0));
-               m_leftFlyWheelMotor.Set(SmartDashboard::GetNumber("DB/Slider 2",0.0));
-           }
-           else
-           {
-               m_rightFlyWheelMotor.Set(-m_joystick.GetY());
-               m_leftFlyWheelMotor.Set(-m_joystick.GetY());
-           }
+                    // INDEXER
+                    // A: Indexer quarter rotation
+                    // Gamepad Y: Run indexer
+                    if (m_gamepad.GetRawButton(DriveStationConstants::buttonA)){
+                        m_indexerMotor.goDistance(250,0.5);
+
+                   }
+                    else{
+                        m_indexerMotor.Set(m_gamepad.GetY());
+                    }
+
+                   std::ostringstream indexerEncoderValue;
+                   indexerEncoderValue << "EncoderI: ";
+                   indexerEncoderValue << m_feederMotor.GetEncPosition();
+                   SmartDashboard::PutString("DB/String 8", indexerEncoderValue.str());
+
+                   // FLYWHEELS
+                   // X: Run flywheels off driverstation from slider 1 and 2 input
+                   // Joystick Y: Run flywheels
+                   if (m_gamepad.GetRawButton(DriveStationConstants::buttonX)){
+
+                       // Power-based
+        //               m_topFlyWheelMotor.Set(SmartDashboard::GetNumber("DB/Slider 1",0.0));
+        //               m_lowerFlyWheelMotor.Set(SmartDashboard::GetNumber("DB/Slider 2",0.0));
+
+                       // Velocity-based (implement only after PID tuning of flywheel motors)
+
+                       m_topFlyWheelMotor.goAt(SmartDashboard::GetNumber("DB/Slider 1",0.0));
+                       m_lowerFlyWheelMotor.goAt(SmartDashboard::GetNumber("DB/Slider 2",0.0));
+
+
+                   }
+                   else
+                   {
+                       m_topFlyWheelMotor.Set(-m_joystick.GetY());
+                       m_lowerFlyWheelMotor.Set(-m_joystick.GetY());
+                   }
 
 
 
-           std::ostringstream shooterRightEncoderString;
-           shooterRightEncoderString << "EncoderShR: ";
-           shooterRightEncoderString << m_rightFlyWheelMotor.GetEncPosition();
-           SmartDashboard::PutString("DB/String 4", shooterRightEncoderString.str());
+                   std::ostringstream shooterRightEncoderString;
+                   shooterRightEncoderString << "EncoderTop: ";
+                   shooterRightEncoderString << m_topFlyWheelMotor.GetEncPosition();
+                   SmartDashboard::PutString("DB/String 4", shooterRightEncoderString.str());
 
-           std::ostringstream shooterLeftEncoderString;
-           shooterLeftEncoderString << "EncoderShL: ";
-           shooterLeftEncoderString << m_leftFlyWheelMotor.GetEncPosition();
-           SmartDashboard::PutString("DB/String 5", shooterLeftEncoderString.str());
+                   std::ostringstream shooterLeftEncoderString;
+                   shooterLeftEncoderString << "EncoderLow: ";
+                   shooterLeftEncoderString << m_lowerFlyWheelMotor.GetEncPosition();
+                   SmartDashboard::PutString("DB/String 5", shooterLeftEncoderString.str());
 
-           std::ostringstream lidarString;
-           lidarString << "Distance: ";
-           lidarString << m_lidar.getFastAverage();
-           SmartDashboard::PutString("DB/String 9", lidarString.str());
+                   std::ostringstream lidarString;
+                   lidarString << "Distance: ";
+                   lidarString << m_lidar.getFastAverage();
+                   SmartDashboard::PutString("DB/String 9", lidarString.str());
+
+                   std::ostringstream shooterTopVel;
+                   shooterTopVel << "VelTop: ";
+                   shooterTopVel << m_topFlyWheelMotor.GetEncVel();
+                   //shooterTopVel << SmartDashboard::GetNumber("DB/Slider 1", 0.0);
+                   SmartDashboard::PutString("DB/String 10", shooterTopVel.str());
+
+                   std::ostringstream shooterLowVel;
+                   shooterLowVel << "VelLow: ";
+                   shooterLowVel << m_lowerFlyWheelMotor.GetEncVel();
+                   //shooterLowVel << SmartDashboard::GetNumber("DB/Slider 2", 0.0);
+                   SmartDashboard::PutString("DB/String 11", shooterLowVel.str());
 
 
-           m_configEditor.update();
+                   m_configEditor.update();
 
 
 
-        }
+                }
 
     }
 
