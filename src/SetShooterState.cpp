a0f1858f855451c638fc930f542564d4af7f605f
Brin Harper <brin.harper@team2342.org>
2017-03-06 21:18:57 -0500

    Create ShooterState action

---
+++ src/SetShooterState.cpp
@@ -0,0 +1,42 @@
+/*
+ * SetShooterState.cpp
+ *
+ *  Created on: Mar 6, 2017
+ *      Author: Brin Harper
+ */
+
+#include "SetShooterState.h"
+#include "../Robot.h"
+
+SetShooterState::SetShooterState(int state, vector<shared_ptr<dependency>> dependencies, shared_ptr<Robot> robot) :
+Action (dependencies, robot),
+m_state(state)
+{
+    if (m_state == 0) {
+        m_flywheelState = FlyWheels::STATE::OFF;
+        m_feederState = Feeder::State::OFF;
+    } else {
+        m_flywheelState = FlyWheels::STATE::FLATRATE;
+        m_feederState = Feeder::State::ON;
+    }
+}
+
+SetShooterState::SetShooterState(json& action, shared_ptr<Robot> robot) :
+        Action(),
+        m_state(action["state"])
+{
+    if (m_state == 0) {
+        m_flywheelState = FlyWheels::STATE::OFF;
+        m_feederState = Feeder::State::OFF;
+    } else {
+        m_flywheelState = FlyWheels::STATE::FLATRATE;
+        m_feederState = Feeder::State::ON;
+    }
+}
+
+void SetShooterState::run() {
+
+    m_robot->m_flywheel.setState(m_flywheelState);
+    m_robot->m_feeder.setState(m_feederState);
+
+}
---
+++ src/SetShooterState.h
@@ -0,0 +1,37 @@
+/*
+ * SetShooterState.h
+ *
+ *  Created on: Mar 6, 2017
+ *      Author: Brin Harper
+ */
+
+#ifndef SRC_SETSHOOTERSTATE_H_
+#define SRC_SETSHOOTERSTATE_H_
+
+#include "Action.h"
+#include "dependency.h"
+#include <vector>
+#include <iostream>
+#include "../plog/Log.h"
+
+using namespace std;
+class Robot;
+
+class SetShooterState : public Action
+{
+    public:
+        SetShooterState(int state, vector<shared_ptr<dependency>> dependencies, shared_ptr<Robot> robot);
+        SetShooterState(json& action, shared_ptr<Robot> robot);
+
+    private:
+        int m_state;
+        enum FlyWheels::STATE m_flywheelState;
+        enum Feeder::State m_feederState;
+        void run();
+
+};
+
+
+
+
+#endif /* SRC_SETSHOOTERSTATE_H_ */
